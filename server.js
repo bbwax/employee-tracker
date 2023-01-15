b6b4bda94046eed7906c9340efbc34fc0492f8e7
bwax <braxton.waxdeck@gmail.com>
2023-01-14 17:48:36 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DB,
+    password: process.env.DB_PASS
+});
