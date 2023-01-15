8c98a61d7e9c6d8b588f84002edbca8c2fc92750
bwax <braxton.waxdeck@gmail.com>
2023-01-14 22:31:19 -0800

    connected database, added inquirer questions

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const inquirer = require('inquirer');
+require("dotenv").config();
 const mysql = require('mysql2');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -6,3 +17,70 @@ const connection = mysql.createConnection({
     database: process.env.DB_DB,
     password: process.env.DB_PASS
 });
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Connected to the database!");
+    start();
+});
+
+
+const start = () => {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Quit"
+            ]
+        })
+        .then(answer => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "Quit":
+                    connection.end();
+                    break;
+            }
+        });
+
+};
+
+
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
